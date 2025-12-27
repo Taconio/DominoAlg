18e3b48adbd068d7bf897b9c2d348d0ddcfef5ba
tacticalshorts <anthonygilrod@gmail.com>
2025-12-26 19:35:13 -0500

    board empty checking

--- domino.py
+++ domino.py
@@ -1,4 +1,6 @@
 from typing import List, Tuple, Literal
+from collections import deque
+
 # Domino Game imlpementation, for now just making a simple functional domino game
 
 # TODO today:
@@ -7,15 +9,35 @@ from typing import List, Tuple, Literal
 Tile = Tuple[int, int]
 Side = Literal["L", "R"]
 
+# Helpers
 def make_set_double6() -> List[Tile]:
     # Unordered tiles: (a, b) where 0 <= a <= b <= 6
     return [(a, b) for a in range(7) for b in range(a, 7)]
+
+def flip(t: Tile) -> Tile:
+    return (t[1], t[0])
 
 # Used to hold info about the player and hand
 class Player:
     name: str
     team: int
     hand: list[Tile]
+
+# TODO: placing a piece, checking for an empty list
+class Board:
+    chain: deque = field(default_factory=deque)
+    left_end: Optional[int] = None
+    right_end: Optional[int] = None
+
+    def is_empty(self) -> bool:
+        return len(self.chain) == 0
+
+
+
+
+# TODO: Track games
+class Game:
+    
 
 
 def main():
